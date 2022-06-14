b0bb1c8e5155d84c63701ea42c4b6996aa3922d4
serendipity <sju0924@khu.ac.kr>
2022-06-14 14:12:05 +0900

    오타수정

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_data')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 def youn(request):
     return render(request, 'blog/youn.html', {})
