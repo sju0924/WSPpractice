59b442a769dbe10b1a30561c185e7c14f3909ed6
serendipity <sju0924@khu.ac.kr>
2022-06-14 15:12:18 +0900

    add form

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path(r'', views.youn),
-    path('post_list/', views.post_list),
+    path(r'', views.post_list),
     path('post/<int:pk>/', views.post_detail, name='post_detail'), 
+    path('post/new/', views.post_new, name='post_new'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -13,3 +13,7 @@ def youn(request):
 def post_detail(request,pk):
     post = get_object_or_404(Post,pk=pk)
     return render(request, 'blog/post_detail.html', {'post':post})
+
+def post_new(request):
+    form = PostForm() 
+    return render(request, 'blog/post_edit.html', {'form': form})
