0757a482599189994efe1bc09a5b731cb64ddff0
serendipity <sju0924@khu.ac.kr>
2022-06-14 14:02:43 +0900

    날아간거 복구:

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-     path('/', views.youn, name='index'),
+    path('/', views.youn, name='index'),
     path('/post_list', views.post_list, name='post_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
+import imp
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_data')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 def youn(request):
     return render(request, 'blog/youn.html', {})
