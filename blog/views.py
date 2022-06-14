0808dc1f2aecf32d00e462854dba7884eea0309f
serendipity <sju0924@khu.ac.kr>
2022-06-14 15:16:50 +0900

    오타 수정

--- blog/views.py
+++ blog/views.py
@@ -25,8 +25,8 @@ def post_new(request):
             post.published_date= timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
-        else:
-            form = PostForm()
- 
+    else:
+        form = PostForm()
+
     return render(request, 'blog/post_edit.html', {'form': form})
 
