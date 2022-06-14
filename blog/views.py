1422217aec34bda79979d3929c80de16ba0a4d18
serendipity <sju0924@khu.ac.kr>
2022-06-14 13:50:21 +0900

    main/post_list 분리

--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+def youn(request):
+    return render(request, 'blog/youn.html', {})
