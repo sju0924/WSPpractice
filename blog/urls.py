99b45ea5558c0e32abdf0a4e4fec6aa2b9f25a1c
serendipity <sju0924@khu.ac.kr>
2022-06-14 14:57:36 +0900

    게시글 상세보기 추가

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path(r'', views.youn),
     path('post_list/', views.post_list),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'), 
 ]
--- blog/views.py
+++ blog/views.py
@@ -10,3 +10,6 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts': posts})
 def youn(request):
     return render(request, 'blog/youn.html', {})
+def post_detail(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
